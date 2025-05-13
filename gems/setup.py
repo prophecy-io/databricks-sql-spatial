4eb5ef51df94149ed552dd1be45850fa2b525deb
abhinavjain <abhinav@simpledatalabs.com>
2025-05-13 20:49:59 +0530

    adding createpoint and distance gem

---
+++ gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'DatabricksSqlBasics',
+    version = '0.0.5.dev2',
+    packages = ['DatabricksSqlBasics'],
+    package_dir = {'DatabricksSqlBasics': '.'},
+    description = '',
+    install_requires = [],
+)
+
