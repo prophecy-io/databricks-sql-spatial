2906f341f02b70849bb0ef1c0ded3ee8a71df819
andre <mesarovic.andreprophecy.com@andres-MacBook-Pro-2.local>
2025-07-10 18:39:50 -0400

    First commit for GeneralizeTool gem. Macro has original UDF query

---
+++ macros/GeneralizeTool.sql
@@ -0,0 +1,11 @@
+{%- macro GeneralizeTool(table_name,schema,polygonColumnName,threshold,unit) -%}
+    {{ log("table_name=" ~ table_name, info=True) }}
+    {{ log("schema=" ~ schema, info=True) }}
+    {{ log("polygonColumnName=" ~ polygonColumnName, info=True) }}
+    {{ log("threshold=" ~ threshold, info=True) }}
+    {{ log("unit=" ~ unit, info=True) }}
+
+    select
+    andre_dev.alteryx_spatial.generalize({{polygonColumnName}}, {{threshold}}, True) as output
+    from {{ table_name }}
+{%- endmacro -%}
