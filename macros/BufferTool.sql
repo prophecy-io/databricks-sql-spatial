0fc6e95429089d62ca62de1ea09ec3f59bd71a4e
andre <mesarovic.andreprophecy.com@andres-MacBook-Pro-2.local>
2025-07-10 12:28:06 -0400

    First commit for BufferTool

---
+++ macros/BufferTool.sql
@@ -0,0 +1,26 @@
+{%- macro BufferTool(table_name,schema,polygonColumnName,distance,unit) -%}
+  {{ log("table_name=" ~ table_name, info=True) }}
+  {{ log("schema=" ~ schema, info=True) }}
+  {{ log("polygonColumnName=" ~ polygonColumnName, info=True) }}
+  {{ log("distance=" ~ distance, info=True) }}
+  {{ log("unit=" ~ unit, info=True) }}
+SELECT
+ ST_AsText(
+  ST_Transform(
+   ST_Buffer(
+    ST_Transform(
+     ST_GeomFromText(
+      {{polygonColumnName}},
+      4326
+     ),
+     3857
+    ),
+    {{distance}}
+   ),
+   4326
+  )
+ ) as output
+FROM
+ {{table_name}}
+
+{%- endmacro -%}
