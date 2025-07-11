9da94abc661110cffa93fc2ccbe9c8f85bdd2784
andre <mesarovic.andreprophecy.com@andres-MacBook-Pro-2.local>
2025-07-11 17:07:51 -0400

    Generalize.sql macro: changed query from UDF to ST_Simplify

--- macros/Generalize.sql
+++ macros/Generalize.sql
@@ -1,18 +1,18 @@
-{%- macro Generalize(table_name,schema,polygonColumnName,threshold,unit) -%}
+{%- macro Generalize(table_name, schema, polygonColumnName, threshold, unit) -%}
     {{ log("table_name=" ~ table_name, info=True) }}
     {{ log("schema=" ~ schema, info=True) }}
     {{ log("polygonColumnName=" ~ polygonColumnName, info=True) }}
     {{ log("threshold=" ~ threshold, info=True) }}
     {{ log("unit=" ~ unit, info=True) }}
 
-    select
+    SELECT 
       {{polygonColumnName}} as input,
-      andre_dev.alteryx_spatial.generalize(
-        {{polygonColumnName}}, 
-        {{threshold}}, 
-        True
+      ST_AsText(
+          ST_Simplify(
+            ST_GeomFromText({{polygonColumnName}}), 
+            {{threshold}}
+          ) 
       ) as output
-      from 
-        {{ table_name }}
+    FROM 
+      {{table_name}}
 {%- endmacro -%}
-
