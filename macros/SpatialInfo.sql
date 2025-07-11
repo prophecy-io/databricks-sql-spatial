b7252a993ace869ad1fe9b683e47fb7a4a83cdc6
andre <mesarovic.andreprophecy.com@andres-MacBook-Pro-2.local>
2025-07-11 18:47:51 -0400

    SpatialInfo.sql: added ST_Centroid to query

--- macros/SpatialInfo.sql
+++ macros/SpatialInfo.sql
@@ -1,13 +1,13 @@
-{%- macro SpatialInfo(table_name,schema,polygonColumnName,distance,unit) -%}
+{%- macro SpatialInfo(table_name, schema, polygonColumnName, area) -%}
   {{ log("table_name=" ~ table_name, info=True) }}
   {{ log("schema=" ~ schema, info=True) }}
   {{ log("polygonColumnName=" ~ polygonColumnName, info=True) }}
-  {{ log("distance=" ~ distance, info=True) }}
-  {{ log("unit=" ~ unit, info=True) }}
+  {{ log("area=" ~ arexa, info=True) }}
 
   SELECT
+    ST_AsText(ST_Centroid(ST_GeomFromText({{polygonColumnName}}))) as centroid,
     {{polygonColumnName}} as input
   FROM
     {{table_name}}
-
+  
 {%- endmacro -%}
