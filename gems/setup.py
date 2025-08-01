4e64be3c8cfb4195464570b2d21645539001d21a
abhinavjain <abhinav@simpledatalabs.com>
2025-08-01 10:53:55 +0530

    prod tag 0.0.4

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'DatabricksSqlSpatial',
-    version = '0.0.4.dev0',
+    version = '0.0.4',
     packages = ['DatabricksSqlSpatial'],
     package_dir = {'DatabricksSqlSpatial': '.'},
     description = '',
